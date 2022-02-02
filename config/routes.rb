33be3bf2dac60b922041ce1e8a70b74f3a2e79a5
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-01 17:52:19 -0700

    feature: create fighters_controller, organizations_controller, organization model, organizations index view, create organization migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/hotels', to:'hotels#index'
+  get '/mma_organizations', to:'organizations#index'
+  get '/fighters', to:'fighters#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_223628) do
+ActiveRecord::Schema.define(version: 2022_02_01_235834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2022_02_01_223628) do
     t.datetime "updated_at"
     t.boolean "five_stars"
     t.integer "year_founded"
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "name"
+    t.integer "year_founded"
+    t.string "president"
+    t.boolean "international"
   end
 
 end
