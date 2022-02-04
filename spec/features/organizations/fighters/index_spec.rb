443673bde3aaa22e7b82db6cd8d83620e11e2903
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-04 08:41:49 -0700

    create fighters spec folder inside of organizations. Create index_spec.rb and add userstory5 comment

---
+++ spec/features/organizations/fighters/index_spec.rb
@@ -0,0 +1,6 @@
+# User Story 5
+require 'rails_helper'
+
+# As a visitor
+# When I visit '/organization/:id/fighters'
+# Then I see each fighter that is associated with that organizatioin with each fighter's attributes:
