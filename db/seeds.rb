b94fccfc7df59d7eb786910a6d6399fdf457a274
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-04 16:59:36 -0700

    add destroy_all to seeds, add more data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,18 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+Organization.destroy_all
+Fighter.destroy_all
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@organization = Organization.create!(name: "UFC", year_founded: 1995, president: "Dana White", international: true)
-@fighter_1 = @organization.fighters.create!(name: "Becky", height: 14.0, weight:120.0, reach: 36, stance: "orthodox", active: true)
-@fighter_2 = @organization.fighters.create!(name: "Carlos Condit", height: 2.2, weight:170.0, reach: 70, stance: "orthodox", active: false)
+ufc = Organization.create!(name: "UFC", year_founded: 1995, president: "Dana White", international: true)
+bellator = Organization.create!(name: "Bellator", year_founded: 2002, president: "Scott Coker", international: false)
+becky = ufc.fighters.create!(name: "Becky", height: 14.0, weight:120.0, reach: 36, stance: "orthodox", active: true)
+carlos = ufc.fighters.create!(name: "Carlos Condit", height: 2.2, weight:170.0, reach: 70, stance: "orthodox", active: false)
+
+chandler = bellator.fighters.create!(name: "Michael Chandler", height: 1.7, weight:155.0, reach: 65, stance: "orthodox", active: true)
+venom = bellator.fighters.create!(name: "Michael Venom Page", height: 2.3, weight:165.0, reach: 70, stance: "southpaw", active: true)
 
 @hotel = Hotel.create!(name: 'Hilton', five_stars: true, year_founded: 1980)
 @suite_1 = Suite.create(name: '120', clean: true, number_of_beds: 4, hotel_id: hotel.id)
