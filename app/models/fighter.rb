0e558f3696066113086087b84a57279f40c6b437
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-03 17:52:29 -0700

    refactor tests to test for validation and relationship. Tests pass

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,3 +1,12 @@
 class Fighter < ApplicationRecord
     belongs_to :organization
+    # validates_presence_of :id
+    validates_presence_of :name
+    # validates_presence_of :created_at
+    # validates_presence_of :updated_at
+    validates_presence_of :height
+    validates_presence_of :weight
+    validates_presence_of :reach
+    validates_presence_of :stance
+    validates_presence_of :active
 end 
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,7 @@
 class Organization < ApplicationRecord
     has_many :fighters
+    validates_presence_of :name
+    validates_presence_of :year_founded
+    validates_presence_of :president
+    validates_presence_of :international
 end 
