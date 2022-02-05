f7015fda1cd5e6895bcab23b06e9d79bcd625189
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-04 18:20:32 -0700

    test: #self.in order pass

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do 
+    before :each do 
+        @organization_1 = Organization.create!(name: "UFC", year_founded: 1995, president: "Dana White", international: true)
+        @organization_2 = Organization.create!(name: "Bellator", year_founded: 2002, president: "Scott Coker", international: true)
+    end 
     describe 'validations' do 
         it {should validate_presence_of :name}
         it {should validate_presence_of :year_founded}
@@ -11,4 +15,14 @@ RSpec.describe Organization, type: :model do
     describe 'relationship' do 
         it {should have_many :fighters}
     end 
-end 
+
+    describe 'instance methods' do 
+        it '#self.in order' do 
+            organization_1_ordered_index = Organization.all.in_order[1]
+            organization_2_ordered_index = Organization.all.in_order[0]
+            expect(@organization_2).to eq(Organization.all.last)
+            expect(organization_2_ordered_index).to eq (Organization.all.in_order.first)
+        end
+    end 
+end  
+
