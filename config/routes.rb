45f89e9e6dd6bb0d5041f75e0c76983c4b00d797
Becky Nisttahuz <benistta@hotmail.com>
2022-02-05 15:44:25 -0700

    updated routes for hotel_suites

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get 'hotel/:id', to:'hotels#show'
   get 'fighter/:id', to:'fighters#show'
   get '/suite/:id', to: 'suites#show'
+  get '/hotel/:id/suites', to:'hotel_suites#index'
 end
