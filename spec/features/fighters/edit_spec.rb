c3e303bef80cfd036a75bccb3242cd47a3a57bdc
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-06 17:40:05 -0700

    test: Write tests for update fighter: can navigate to update form, can successfully update a fighter

--- spec/features/fighters/edit_spec.rb
+++ spec/features/fighters/edit_spec.rb
@@ -16,5 +16,18 @@ it 'can navigate the user to an update form' do
     visit "/fighters/#{fighter.id}"
     click_link "Update Rocky"
     expect(current_path).to eq("/fighters/#{fighter.id}/edit")
-end 
+    end
+    
+it 'can successfully update a fighter' do
+    organization = Organization.create!(name: "Rollie Pollie", year_founded: 1994, president: "Bam Bam", international: true)
+    fighter = organization.fighters.create!(name: "Rcky", height: 4435.0, weight: 190.0, reach: 36, stance: "switch", active: false)
+    visit "/fighters/#{fighter.id}"
+    expect(page).to have_content("Rcky")
+    visit "/fighters/#{fighter.id}/edit"
+    fill_in 'name', with: "Rocky"
+    fill_in 'height', with: "2.1"
+    click_button "Update Fighter"
+    expect(current_path).to eq("/fighters/#{fighter.id}")
+    expect(page).to have_content("Rocky")
+    end
 end
