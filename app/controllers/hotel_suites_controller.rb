e4f9cdb42bb93adfd9d9369d54b4624240c28187
Becky Nisttahuz <benistta@hotmail.com>
2022-02-07 14:48:11 -0700

    Feat: Created methods new and suite params

--- app/controllers/hotel_suites_controller.rb
+++ app/controllers/hotel_suites_controller.rb
@@ -4,9 +4,18 @@ class HotelSuitesController < ApplicationController
     @suites = @hotel.suites
   end
 
+  def new
+    @hotel = Hotel.find(params[:id])
+  end
+
   def create
     hotel = Hotel.find(params[:id])
     suite = hotel.suites.create!(suite_params)
     redirect_to "/hotels/#{hotel.id}/suites"
 end
+
+private
+  def suite_params
+    params.permit(:name, :clean, :number_of_beds)
+  end
 end
