50be039aa29e2893407c40ec441b8350608e76e8
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-07 19:59:54 -0700

    test: write test for has a link that can sort fighters in alphabetical order

--- spec/features/organizations/fighters/index_spec.rb
+++ spec/features/organizations/fighters/index_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Organization Fighter's Index" do
     before :each do 
         @organization_1 = Organization.create!(name: "UFC", year_founded: 1995, president: "Dana White", international: true)
         @organization_2 = Organization.create!(name: "Bellator", year_founded: 2002, president: "Scott Coker", international: true)
-        @fighter_1 = @organization_1.fighters.create!(name: "Becky", height: 14.0, weight:120.0, reach: 36, stance: "orthodox", active: true)
+        @fighter_1 = @organization_1.fighters.create!(name: "Zelda", height: 14.0, weight:120.0, reach: 36, stance: "orthodox", active: true)
         @fighter_2 = @organization_1.fighters.create!(name: "Carlos Condit", height: 2.2, weight:170.0, reach: 70, stance: "orthodox", active: true)
     end
 
@@ -17,4 +17,11 @@ RSpec.describe "Organization Fighter's Index" do
         expect(page).to have_content(@fighter_1.name)
         expect(page).to have_content(@fighter_2.name)
     end
+
+    it 'has a link that can sort fighters in alphabetical order' do 
+        visit "organizations/#{@organization_1.id}/fighters" 
+        expect(@fighter_1.name).to appear_before(@fighter_2.name)
+        click_link "Alphebetize Fighters"
+        expect(@fighter_2.name).to appear_before(@fighter_1.name)
+    end 
 end 
